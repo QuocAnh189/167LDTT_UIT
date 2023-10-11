cf7154f391c44b2f1f694bd59f9793cb53120472
QuocAnh189 <anhquoc18092003@gmail.com>
2023-10-11 10:37:21 +0700

    update

--- 215121360_LĐTT/Bai001/Bai001.cpp
+++ 215121360_LĐTT/Bai001/Bai001.cpp
@@ -1,22 +1,23 @@
-#include<iostream>
+#include <iostream>
+#include <cmath>
+
 using namespace std;
 
 int main()
 {
-	float x1;
-	cout << "Nhap x1: ";
+	float x1, y1;
+	cout << "Enter x1: ";
 	cin >> x1;
-	float y1;
-	cout << "Nhap y1: ";
+	cout << "Enter y1: ";
 	cin >> y1;
-	float x2;
-	cout << "Nhap x2: ";
+
+	float x2, y2;
+	cout << "Enter x2: ";
 	cin >> x2;
-	float y2;
-	cout << "Nhap y2: ";
+	cout << "Enter y2: ";
 	cin >> y2;
 
 	float d = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
-	cout << "Khoang cach: " << d;
+	cout << "Distance: " << d;
 	return 0;
 }
